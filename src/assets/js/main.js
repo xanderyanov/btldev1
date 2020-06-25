26bb7fddff1b2e6dfd43f5ae142a9fd7189eae67
xanderyanov <xanderyanov@gmail.com>
2020-06-25 18:27:10 +0400

    25-06-2020-18-27

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,4 +2,49 @@
 
 // });
 
-$(function () {});
+$(function () {
+  // главный слайдер на главной
+  if ($(".swiper-container1").length) {
+    var mySwiper1 = new Swiper(".swiper-container1", {
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination1",
+        type: "bullets",
+        dynamicBullets: false,
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+      },
+      on: {
+        init: function () {
+          console.log("swiper initialized");
+          $(".swiper-slide").children(".swiper__cadr").removeClass("animationfadeIn").fadeOut(500);
+          setTimeout(function () {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("animationfadeIn");
+          }, 500);
+        },
+        slideChange: function () {
+          $(".swiper-slide").children(".swiper__cadr").removeClass("animationfadeIn").fadeOut(500);
+          setTimeout(function () {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("animationfadeIn");
+          }, 500);
+        },
+      },
+    });
+  }
+});
