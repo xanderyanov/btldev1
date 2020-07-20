fe04e985867b050ba696e6209b5e40f5f6d5038b
xanderyanov <xanderyanov@gmail.com>
2020-07-20 18:08:03 +0400

    20-07-20-18-07

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -103,6 +103,7 @@ $(function () {
   $(".phoneZ").mask("+7 (999) 999-9999");
   $(".phone1").mask("+7 (999) 999-9999");
   $(".phone2").mask("+7 (999) 999-9999");
+  $(".phone3").mask("+7 (999) 999-9999");
 
   // прокрутка доя якоря на странице процедуры
   $("#goToPriceAnchor").on("click", function (e) {
@@ -191,8 +192,92 @@ $(function () {
   });
   $(".menuCategoryMastersLink__all").on("click", function (e) {
     e.preventDefault();
-    $(this).addClass("active");
+
     $(".mastersBox__itemOuter").removeClass("hidden");
     $(".mastersBox__itemOuter").addClass("visible");
+    $(this).addClass("active");
   });
 });
+
+var isMobile = {
+  Android: function () {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function () {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function () {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function () {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function () {
+    return navigator.userAgent.match(/IEMobile/i);
+  },
+  any: function () {
+    return isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows();
+  },
+};
+
+if ($(".map__area").length) {
+  ymaps.ready(init);
+
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [51.53636907237114, 46.022191999999926],
+      zoom: 14,
+      controls: ["zoomControl"],
+    });
+
+    myMap.behaviors.disable("scrollZoom");
+    if (isMobile.any()) {
+      myMap.behaviors.disable("drag");
+    }
+
+    var myGeoObjects = [];
+
+    myGeoObjects[0] = new ymaps.Placemark(
+      [51.53636907237114, 46.022191999999926],
+      {
+        balloonContentHeader:
+          '<div class="baloon__top">"БЬЮТИ Лаунж"</div>' +
+          '<div class="baloon__description">Центр косметологии и красоты</div>',
+        balloonContentBody:
+          '<div class="baloon__content"><img src="assets/img/logo-map2.png">' +
+          '<a href="tel:88452260000">8(8452) 26-00-00</a>',
+        balloonContentFooter: '<div class="baloon__footer">Саратов, ул. Московская д.117, <br>ТК «Мир», 2 этаж.</div>',
+        clusterCaption: "Косметология <br>салон массажа<br>HAIR услуги<br>NAIL-BAR<br>профессиональная косметика",
+        hintContent: '<div class="baloon__top">Центр косметологии и красоты "БЬЮТИ Лаунж"</div>',
+      },
+      {
+        iconLayout: "default#image",
+        iconImageHref: "assets/img/marker.png",
+        iconImageSize: [30, 48],
+        iconImageOffset: [-15, -48],
+      }
+    );
+
+    var clusterIcons = [
+      {
+        href: "/images/pointer.png",
+        size: [29, 46],
+        offset: [0, 0],
+      },
+    ];
+
+    var clusterer = new ymaps.Clusterer({
+      clusterDisableClickZoom: false,
+      clusterOpenBalloonOnClick: false,
+      clusterBalloonContentLayout: "cluster#balloonCarousel",
+      clusterBalloonPanelMaxMapArea: 0,
+      clusterBalloonContentLayoutWidth: 300,
+      clusterBalloonContentLayoutHeight: 200,
+      clusterBalloonPagerSize: 5,
+      clusterBalloonPagerType: "marker",
+    });
+
+    clusterer.add(myGeoObjects);
+    myMap.geoObjects.add(clusterer);
+  }
+}
