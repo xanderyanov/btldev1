3fd3f42daddb12836124169be9e195cc8b63906b
xanderyanov <zokrat@yandex.ru>
2021-02-19 17:58:46 +0400

    17-59

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,6 +4,28 @@
 
 $(function () {
 
+
+  // var $iframes = $( "iframe" );
+  // $iframes.each(function () {
+  //   $( this ).data( "ratio", this.height / this.width )
+  //     .removeAttr( "width" )
+  //     .removeAttr( "height" );
+  // });
+  // $( window ).resize( function () {
+  //   $iframes.each( function() {
+  //     var width = $( this ).parent().width();
+  //     $( this ).width( width )
+  //       .height( width * $( this ).data( "ratio" ) );
+  //   });
+  // }).resize();
+
+
+
+
+
+
+
+
   // START всплывающий прайс по клику на Р
   $(".innerPriceBtn").on("click", function (e) {
     e.preventDefault();
