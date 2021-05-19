b274afb41bb82fb8b3c852e022e2da2827bc027e
xanderyanov <xanderyanov@gmail.com>
2021-05-19 17:39:53 +0400

    cabinetPage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -65,6 +65,40 @@ $(function () {
     }
   });
 
+  /****************** Табы личного кабинета*/
+
+  if ($(window).width() <= 768) {
+    // $('.cab2__tabs').children('li').first().children('a').addClass('active').next().addClass('open').show();
+    $(".cab2__tabs").on("click", "li > a", function (e) {
+      e.preventDefault();
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this).next().removeClass("openSection").slideUp();
+      } else {
+        $(".cab2__tab > a").removeClass("active");
+        $(this).addClass("active");
+        $(".cab2__tab > section").removeClass("openSection").slideUp();
+        $(this).next().addClass("openSection").slideDown();
+      }
+    });
+  } else {
+    $(".cab2__tabs").children("li").first().children("a").addClass("active").next().addClass("openSection").show();
+    $(".cab2__tabs").on("click", "li > a", function (e) {
+      e.preventDefault();
+      if ($(this).hasClass("active")) {
+        // $(this).removeClass('active');
+        // $(this).next().removeClass('openSection').hide();
+      } else {
+        $(".cab2__tab > a").removeClass("active");
+        $(this).addClass("active");
+        $(".cab2__tab > section").removeClass("openSection").hide();
+        $(this).next().addClass("openSection").show();
+      }
+    });
+  }
+
+  /****************** */
+
   // $(window).on("navigate", function (event, data) {
   //   var direction = data.state.direction;
   //   if (direction == "back") {
