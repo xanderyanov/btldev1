042075ae286574249a0dd55e47c1dc964a513eeb
xanderyanov <xanderyanov@gmail.com>
2021-05-27 17:56:05 +0400

    17-55

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -75,6 +75,33 @@ $(function () {
 
   /*end-brandsPage*/
 
+  /*start-newSlideMobileMenu*/
+
+  $(".leftSlideMenu_js").on("click", function (e) {
+    e.preventDefault();
+    $(".leftSlideMenu__overlay").fadeIn();
+    $(".leftSlideMenu__wrapper").addClass("leftSlideMenu__wrapper_open");
+    $("body").addClass("stop");
+  });
+
+  $(".leftSlideMenu__close").on("click", function (e) {
+    e.preventDefault();
+    $(".leftSlideMenu__wrapper").removeClass("leftSlideMenu__wrapper_open");
+    $(".leftSlideMenu__overlay").fadeOut();
+    $("body").removeClass("stop");
+  });
+  $(".leftSlideMenu__wrapper").on("click", function (e) {
+    e.stopPropagation();
+  });
+  $(".leftSlideMenu__overlay").on("click", function (e) {
+    e.preventDefault();
+    $(".leftSlideMenu__wrapper").removeClass("leftSlideMenu__wrapper_open");
+    $(".leftSlideMenu__overlay").fadeOut();
+    $("body").removeClass("stop");
+  });
+
+  /*end-newSlideMobileMenu*/
+
   /****************** Табы личного кабинета*/
 
   // $('.cab2__tabs').children('li').first().children('a').addClass('active').next().addClass('open').show();
