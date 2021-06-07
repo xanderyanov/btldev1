285b9b7bd60ef291aea8a2ec909f36abeab09be5
xanderyanov <xanderyanov@gmail.com>
2021-06-07 13:58:29 +0400

    cab2__tabs_script

--- src/assets/js/__resize.js
+++ src/assets/js/__resize.js
@@ -14,16 +14,19 @@ function siteResizeFunction() {
   if (prevWindowWidth > 1024 && windowWidth <= 1024) {
     $(".eShopMenu__outer_catalogOnly").hide();
   }
+
+  //* start - cabinetTabs **/ + __eShopCabinet.js
   if (prevWindowWidth <= 768 && windowWidth > 768) {
-    // $(".cab2__tabs").children("li").first().children("a").addClass("active").next().addClass("open").show();
-    eShopCabTabDesktop();
+    $(".cab2__tab > a").removeClass("active");
+    $(".cab2__tab > section").removeClass("openSection").hide();
+    $(".cab2__tabs").children("li").first().children("a").addClass("active").next().addClass("open").show();
   }
   if (prevWindowWidth > 768 && windowWidth <= 768) {
-    // $(".cab2__tab > a").removeClass("active");
-    // $(".cab2__tab > section").removeClass("openSection").slideUp();
-    // $(".cab2__tabs").children("li").first().children("a").removeClass("active").next().removeClass("open").hide();
-    eShopCabTabMobile();
+    $(".cab2__tab > a").removeClass("active");
+    $(".cab2__tab > section").removeClass("openSection").slideUp();
+    $(".cab2__tabs").children("li").first().children("a").removeClass("active").next().removeClass("open").hide();
   }
+  //* end - cabinetTabs **/
 
   if (prevWindowWidth <= 600 && windowWidth > 600) {
     $(".masterWindowForm__overlay").fadeOut(300);
